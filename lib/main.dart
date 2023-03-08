cc7657bfbf2418f84d1d8d5aafc410ed555aafeb
grey <z1251543107@gmail.com>
2023-03-08 21:11:57 +0800

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -9,6 +12,11 @@ void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -22,29 +30,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print('You are a verified user');
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified){
+                // Already VerifyEmail 
+                print('Email is verified');
               } else {
-                print('You need to verified your email first');
+                // go VerifyEmail
+                return const VerifyEmailView();
               }
-              return const Text('done');
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
+            } else {
+              // user == null,  go Login
+              return const LoginView();
+            }
+            return const Text('done');
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
+
